0af28e8ff6714cb4c1653abab5480c094f380204
Joseph Woodard <jwoodard@mymail.mines.edu>
2018-02-25 16:59:40 -0700

    Decreased eye height, changed cat color to white

--- src/Cat.java
+++ src/Cat.java
@@ -11,7 +11,7 @@ public class Cat {
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
-	private static final int EYE_HEIGHT = 100;
+	private static final int EYE_HEIGHT = 50;
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
@@ -27,7 +27,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.black);
+		g2.setColor(Color.white);
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
 		g2.setColor(Color.blue);
